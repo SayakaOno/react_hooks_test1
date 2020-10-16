dfb40437adff861b408bece221fe0b186287bd13
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-10-15 22:38:02 -0700

    add propTypes to Input component with test

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = 'party') => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 it('renders without error', () => {
   const wrapper = setup();
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
+});
+
+it('does not throw warning with expected props', () => {
+  checkProps(Input, { secretWord: 'party' });
 });
